a05f3b3cbf67f87d99ec523fa21ac0a9ab8729fc
Monogo <piotr.kusztal@monogo.pl>
2022-09-23 15:49:18 +0200

    new-step-checkout module implementation

---
+++ app/code/monogo/module-new-step-checkout/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'monogo_module-new-step-checkout',
+    __DIR__
+);
